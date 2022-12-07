09248444a63e1ccddaa910b51744aa28ee7f3102
Mr. Cool <82129503+DillonC25@users.noreply.github.com>
2022-12-06 20:44:46 -0600

    Create Day03.py

---
+++ Day03.py
@@ -0,0 +1,141 @@
+def part1():
+  file = open("input.txt","r")
+  Lines = file.readlines()
+
+  priority = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52,
+  }
+  total = 0
+  for line in Lines:
+    match= ""
+    firstpart, secondpart = line[:len(line)//2], line[len(line)//2:]
+
+    for i in range(len(firstpart)):
+      if(firstpart[i] in secondpart):
+        match = firstpart[i]
+    total += priority[match]
+  print(total)
+
+
+def part2():
+  
+  file = open("input.txt","r")
+  Lines = file.readlines()
+  priority = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52,
+  }
+  total = 0
+  for i in range(0,298,3):
+    print(i)
+    first = Lines[i]
+    second = Lines[i+1]
+    third = Lines[i+2]
+    for x in range(len(first)-1):
+      if((first[x] in second) & (first[x] in third)):
+        match = first[x]
+    total += priority[match]
+    
+  print(total)
+part2()
